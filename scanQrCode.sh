e63d9be017601d924f9fcdead10f70d4238b233b
Iliandr123 <ilia-scorpion@mail.ru>
2022-09-21 17:33:17 +0500

    feat(scripts): #2: add two scripts for workability of main test

---
+++ scanQrCode.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+QR_CODE_DIRECTORY="$HOME/Library/Android/sdk/emulator/resources"
+CYPRESS_DIRECTORY="$HOME/Documents/nindao-api-tests"
+
+# clear qrcode in resources directory
+cd $QR_CODE_DIRECTORY
+rm custom.png
+# clear qrcode in cypress directory
+cd $CYPRESS_DIRECTORY/cypress/screenshots/login.spec.cy.js
+rm qrcode.png
+
+# make a screenshot of qrcode by cypress
+cd $CYPRESS_DIRECTORY
+npx cypress run --headless --browser chrome
